53b0f235877d1a769da13a7d5397c143bbd6d455
Aditya Thebe <thebeaditya@gmail.com>
2024-01-20 14:22:14 +0545

    feat: add kube-prometheus-stack helm release

--- kubernetes/setup/prometheus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring -f kubernetes/apps/monitoring/prometheus-community/values.yaml
