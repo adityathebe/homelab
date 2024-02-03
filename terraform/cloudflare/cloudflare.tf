08dcb39cb442d7f4879158265a9a0d218d4f6f21
Aditya Thebe <contact@adityathebe.com>
2024-02-03 10:58:10 +0545

    chore: update cloudflare DNS records

--- terraform/cloudflare/cloudflare.tf
+++ terraform/cloudflare/cloudflare.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = ">= 4"
     }
     sops = {
@@ -44,39 +44,84 @@ resource "cloudflare_record" "adguard" {
 }
 
 resource "cloudflare_record" "proxmox" {
-  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name    = "pve.home"
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "pve.home"
   allow_overwrite = true
-  value   = "192.168.1.4"
-  type    = "A"
-  proxied = false
+  value           = "192.168.1.4"
+  type            = "A"
+  proxied         = false
 }
 
 resource "cloudflare_record" "truenas" {
-  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name    = "nas.home"
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "nas.home"
   allow_overwrite = true
-  value   = "192.168.1.51"
-  type    = "A"
-  proxied = false
+  value           = "192.168.1.51"
+  type            = "A"
+  proxied         = false
 }
 
 resource "cloudflare_record" "wildcard" {
-  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name    = "*.home"
-  comment = "For all apps hosted on kubernetes"
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "*.home"
+  comment         = "For all apps hosted on kubernetes"
   allow_overwrite = true
-  value   = "192.168.1.3"
-  type    = "A"
-  proxied = false
+  value           = "192.168.1.3"
+  type            = "A"
+  proxied         = false
 }
 
 resource "cloudflare_record" "homepage" {
-  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name    = "home"
-  comment = "Dashboard"
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "home"
+  comment         = "Dashboard"
   allow_overwrite = true
-  value   = "192.168.1.3"
-  type    = "A"
-  proxied = false
+  value           = "192.168.1.3"
+  type            = "A"
+  proxied         = false
+}
+
+resource "cloudflare_record" "mx_record" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "@"
+  allow_overwrite = true
+  value           = "inbound-smtp.skiff.com"
+  type            = "MX"
+  priority        = 0
+}
+
+resource "cloudflare_record" "skiff_cname_1" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "skiff1._domainkey"
+  allow_overwrite = true
+  value           = "skiff1.cdlo67izhm20v6p.dkim.skiff.com"
+  type            = "CNAME"
+  proxied         = false
+}
+
+resource "cloudflare_record" "skiff_cname_2" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "skiff2._domainkey"
+  allow_overwrite = true
+  value           = "skiff2.cdlo67izhm20v6p.dkim.skiff.com"
+  type            = "CNAME"
+  proxied         = false
+}
+
+resource "cloudflare_record" "spf" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "@"
+  allow_overwrite = true
+  value           = "v=spf1 include:cdlo67izhm20v6p.spf.skiff.com -all"
+  type            = "TXT"
+  proxied         = false
+}
+
+resource "cloudflare_record" "dmarc" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "_dmarc"
+  allow_overwrite = true
+  value           = "v=DMARC1; p=reject; pct=100; adkim=s; aspf=s"
+  type            = "TXT"
+  proxied         = false
 }
