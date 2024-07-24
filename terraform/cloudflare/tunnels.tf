5071c1f14ff505d5b415edca952f0cb18b5d5a18
Aditya Thebe <contact@adityathebe.com>
2024-07-24 19:07:05 +0545

    feat: cloudflare tunnels with terraform

---
+++ terraform/cloudflare/tunnels.tf
@@ -0,0 +1,68 @@
+resource "cloudflare_tunnel" "homelab" {
+  account_id = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
+  name       = "aditya"
+  secret     = data.sops_file.cloudflare_secrets.data["cloudflare_tunnel_secret"]
+}
+
+resource "cloudflare_tunnel_config" "homelab" {
+  account_id = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
+  tunnel_id  = cloudflare_tunnel.homelab.id
+
+  config {
+    ingress_rule {
+      hostname = "movies.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      path     = ""
+      service  = "http://10.99.99.25"
+      origin_request {
+        http_host_header = "movary.home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      }
+    }
+    ingress_rule {
+      hostname = "freshrss.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      path     = ""
+      service  = "http://10.99.99.25"
+      origin_request {
+        http_host_header = "rss.home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      }
+    }
+    ingress_rule {
+      hostname = "music.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      path     = ""
+      service  = "http://10.99.99.25"
+      origin_request {
+        http_host_header = "navidrome.home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+      }
+    }
+    ingress_rule {
+      hostname = "*"
+      service  = "http_status:503"
+    }
+  }
+}
+
+resource "cloudflare_record" "homelab_movary" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "movies"
+  allow_overwrite = true
+  value           = cloudflare_tunnel.homelab.cname
+  type            = "CNAME"
+  proxied         = true
+}
+
+resource "cloudflare_record" "homelab_fresh_rss" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "freshrss"
+  allow_overwrite = true
+  value           = cloudflare_tunnel.homelab.cname
+  type            = "CNAME"
+  proxied         = true
+}
+
+resource "cloudflare_record" "homelab_navidrome" {
+  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name            = "music"
+  allow_overwrite = true
+  value           = cloudflare_tunnel.homelab.cname
+  type            = "CNAME"
+  proxied         = true
+}
