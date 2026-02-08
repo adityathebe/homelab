614e2da859e0a6374907a7a0b19ff2c82c0ad908
Aditya Thebe <contact@adityathebe.com>
2026-02-08 15:18:24 +0545

    chore(terraform): migrate cloudflare provider config to v5
    - Bump cloudflare/cloudflare provider from v4 to v5 and refresh lockfile
    - Rename DNS resources from cloudflare_record to cloudflare_dns_record
    - Remove allow_overwrite and add ttl for DNS records
    - Update tunnel secret argument to tunnel_secret
    - Keep homelab DNS name derived from encrypted homelab_domain

--- terraform/cloudflare/adityathebe.com.tf
+++ terraform/cloudflare/adityathebe.com.tf
@@ -1,54 +1,53 @@
-resource "cloudflare_record" "mx_record_1" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "adityathebe.com"
-  allow_overwrite = true
-  content         = "in1-smtp.messagingengine.com"
-  type            = "MX"
-  priority        = 10
+resource "cloudflare_dns_record" "mx_record_1" {
+  zone_id  = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name     = "adityathebe.com"
+  content  = "in1-smtp.messagingengine.com"
+  type     = "MX"
+  ttl      = 1
+  priority = 10
 }
 
-resource "cloudflare_record" "mx_record_2" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "adityathebe.com"
-  allow_overwrite = true
-  content         = "in2-smtp.messagingengine.com"
-  type            = "MX"
-  priority        = 20
+resource "cloudflare_dns_record" "mx_record_2" {
+  zone_id  = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name     = "adityathebe.com"
+  content  = "in2-smtp.messagingengine.com"
+  type     = "MX"
+  ttl      = 1
+  priority = 20
 }
 
-resource "cloudflare_record" "dkim_cname_1" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "fm1._domainkey"
-  allow_overwrite = true
-  content         = "fm1.adityathebe.com.dkim.fmhosted.com"
-  type            = "CNAME"
-  proxied         = false
+resource "cloudflare_dns_record" "dkim_cname_1" {
+  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name    = "fm1._domainkey.adityathebe.com"
+  content = "fm1.adityathebe.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
 }
 
-resource "cloudflare_record" "dkim_cname_2" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "fm2._domainkey"
-  allow_overwrite = true
-  content         = "fm2.adityathebe.com.dkim.fmhosted.com"
-  type            = "CNAME"
-  proxied         = false
+resource "cloudflare_dns_record" "dkim_cname_2" {
+  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name    = "fm2._domainkey.adityathebe.com"
+  content = "fm2.adityathebe.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
 }
 
-resource "cloudflare_record" "dkim_cname_3" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "fm3._domainkey"
-  allow_overwrite = true
-  content         = "fm3.adityathebe.com.dkim.fmhosted.com"
-  type            = "CNAME"
-  proxied         = false
+resource "cloudflare_dns_record" "dkim_cname_3" {
+  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name    = "fm3._domainkey.adityathebe.com"
+  content = "fm3.adityathebe.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
 }
 
-resource "cloudflare_record" "spf" {
-  zone_id         = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
-  name            = "adityathebe.com"
-  allow_overwrite = true
-  content         = "v=spf1 include:spf.messagingengine.com ?all"
-  type            = "TXT"
-  proxied         = false
+resource "cloudflare_dns_record" "spf" {
+  zone_id = data.sops_file.cloudflare_secrets.data["cloudflare_zone_id"]
+  name    = "adityathebe.com"
+  content = "v=spf1 include:spf.messagingengine.com ?all"
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
 }
-
--- terraform/cloudflare/homelab.tf
+++ terraform/cloudflare/homelab.tf
@@ -1,7 +1,8 @@
-resource "cloudflare_record" "homelab_truenas" {
+resource "cloudflare_dns_record" "homelab_truenas" {
   zone_id = data.sops_file.cloudflare_secrets.data["homelab_cloudflare_zone_id"]
-  name    = "nas"
+  name    = format("nas.%s", data.sops_file.cloudflare_secrets.data["homelab_domain"])
   content = "10.99.99.51"
   type    = "A"
+  ttl     = 1
   proxied = false
 }
