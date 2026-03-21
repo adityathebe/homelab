974a942fa5799df9aa5a2a55a38f36793abd5f19
Aditya Thebe <contact@adityathebe.com>
2026-03-21 14:40:50 +0545

    sunset truenas

--- terraform/cloudflare/homelab.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "homelab_truenas" {
-  zone_id = data.sops_file.cloudflare_secrets.data["homelab_cloudflare_zone_id"]
-  name    = "nas"
-  content = "10.99.99.51"
-  type    = "A"
-  proxied = false
-}
